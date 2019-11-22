1748756dfe243a9b06a636a18a59f54bb4fb5a2a
Ron <ronliu2k@gmail.com>
2019-11-22 23:19:48 +1100

    defined interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export const diff = (l: any, r: any) => {}
+export const diff: Diff = (l, r, options) => {
+  return []
+}
+
+type Option = {
+  path: string | RegExp
+  ignore: boolean
+  array?: {
+    orderSensitive: boolean
+    getKey: <T>(t: T) => string
+  }
+}
+
+type DiffResult =
+  | {
+      type: DiffResultTypeEnum.Added
+      left: any
+    }
+  | { type: DiffResultTypeEnum.Deleted; right: any }
+  | { type: DiffResultTypeEnum.Editted; left: any; right: any }
+
+enum DiffResultTypeEnum {
+  Editted = 'E',
+  Added = 'A',
+  Deleted = 'D'
+}
+
+type Diff = (l: any, r: any, options: Option[]) => DiffResult[]
